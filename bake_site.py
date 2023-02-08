eaf0619de5476853864679b5e74d94fad5676f36
Rutger van Bergen <rbergen@xs4all.nl>
2023-02-08 21:37:34 +0100

    bake_site.py: create data/, bail out on Windows

--- bake_site.py
+++ bake_site.py
@@ -1,5 +1,12 @@
 import subprocess
 import os
+
+if os.name != 'posix':
+    exit()
+
+if not os.path.exists('data'):
+    os.makedirs('data')
+
 os.chdir('site')
 
 subprocess.check_output("npm run build", shell=True)
