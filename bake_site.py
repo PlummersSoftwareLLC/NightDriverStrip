77088453e856b47042f70d53dfe2c80bfffe6b49
Louis Riel <louis-sebastien.riel@ca.ibm.com>
2023-02-10 07:11:41 -0500

    Platformio considerations

--- bake_site.py
+++ bake_site.py
@@ -1,53 +1,36 @@
-import subprocess
-import os
-import sys
-import glob
-from distutils.spawn import find_executable
+localBuild=False
+for i, arg in enumerate(sys.argv):
+    if (arg == 'local'):
+        localBuild=True
 
-def getJsx(folder,mask,exclude=""):
-    files = glob.glob(folder + '/**/' + mask, recursive=True)
-    ret = ""
-    for i, file in enumerate(files):
-        with open(file,'r') as reader:
-            if ((exclude == "") or (file.endswith(exclude) == False)):
-                ret+=reader.read()
-    return ret
+destFolder="data/"
 
-if __name__ == "__main__":
-    localBuild=False
-    for i, arg in enumerate(sys.argv):
-        if (arg == 'local'):
-            localBuild=True
+if (localBuild):
+    destFolder="site/local/"
+    if (os.path.exists(destFolder) == False):
+        print("Creating local build folder")
+        os.mkdir(destFolder)
+else:
+    if (os.path.exists(destFolder) == False):
+        print("Creating chip build folder")
+        os.mkdir(destFolder)
 
-    destFolder="data/"
+jsx = open(destFolder + "main.jsx", "w")
+html = open(destFolder + "index.html", "w")
 
-    if (localBuild):
-        destFolder="site/local/"
-        if (os.path.exists(destFolder) == False):
-            print("Creating local build folder")
-            os.mkdir(destFolder)
-    else:
-        if (os.path.exists(destFolder) == False):
-            print("Creating chip build folder")
-            os.mkdir(destFolder)
+html.write(open('site/src/index.html').read())
+html.close()
 
-    jsx = open(destFolder + "main.jsx", "w")
-    html = open(destFolder + "index.html", "w")
+if (localBuild):
+    print("Local Build")
+    jsx.write(open('site/src/espaddr.jsx').read())
+else:
+    print("Chip Build")
+    jsx.write("const httpPrefix='';")
 
-    html.write(open('site/src/index.html').read())
-    html.close()
-
-    if (localBuild):
-        print("Local Build")
-        jsx.write(open('site/src/espaddr.jsx').read())
-    else:
-        print("Chip Build")
-        jsx.write("const httpPrefix='';")
-
-    jsx.write(open('site/src/modules/modules.jsx').read())
-    jsx.write(getJsx('site/src/theme',"*.jsx"))
-    jsx.write(getJsx('site/src/components',"style.jsx"))
-    jsx.write(getJsx('site/src/components',"*.jsx","style.jsx"))
-    jsx.write(open('site/src/main.jsx').read())
-    print("Build completed")
-
+jsx.write(open('site/src/modules/modules.jsx').read())
+jsx.write(getJsx('site/src/theme',"*.jsx"))
+jsx.write(getJsx('site/src/components',"style.jsx"))
+jsx.write(getJsx('site/src/components',"*.jsx","style.jsx"))
+jsx.write(open('site/src/main.jsx').read())
+print("Build completed")
