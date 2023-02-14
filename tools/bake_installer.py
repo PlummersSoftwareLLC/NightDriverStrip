ff00aed9072012214ce04c9718fd6b9fbde6f5e9
Rutger van Bergen <rbergen@xs4all.nl>
2023-02-14 13:31:47 +0100

    First version of WebInstaller automation

--- tools/bake_installer.py
+++ tools/bake_installer.py
@@ -8,6 +8,7 @@ webinstaller_dir = 'WebInstaller'
 firmware_dir = 'firmware'
 manifests_dir = 'manifests'
 manifest_prefix = 'manifest_'
+manifest_template = 'manifest_template.json'
 webprojects_file = 'web_projects.json'
 
 webprojects_path = os.path.join(config_dir, webprojects_file)
@@ -20,10 +21,14 @@ if not os.path.exists(firmware_path):
     os.makedirs(firmware_path)
 
 f = open(webprojects_path, "r", encoding='utf-8')
-devices = json.load(f)
+json_data = json.load(f)
+devices = json_data['devices']
 f.close()
 
-manifest_source_path = os.path.join(config_dir, manifests_dir)
+f = open(os.path.join(config_dir, manifest_template), "r", encoding='utf-8')
+manifest_text = f.read()
+f.close()
+
 manifest_target_path = os.path.join(webinstaller_dir, manifests_dir)
 if not os.path.exists(manifest_target_path):
     os.makedirs(manifest_target_path)
@@ -41,6 +46,10 @@ for device in devices:
         for bin_file in bin_files:
             shutil.copy(bin_file, project_firmware_path)
 
-        manifest_file_path = os.path.join(manifest_source_path, manifest_prefix + tag + '.json')
-        if os.path.exists(manifest_file_path):
-            shutil.copy(manifest_file_path, manifest_target_path)
+        project_manifest = manifest_text.replace('<name>', project['name'] + ' for ' + device['name'])
+        project_manifest = project_manifest.replace('<chipfamily>', device['chipfamily'])
+        project_manifest = project_manifest.replace('<tag>', tag)
+
+        f = open(os.path.join(manifest_target_path, manifest_prefix + tag + '.json'), 'w', encoding='utf-8')
+        f.write(project_manifest)
+        f.close()
