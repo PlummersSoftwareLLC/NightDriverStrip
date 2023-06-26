3204a85105c4cbbb43951ff7b5f3ee7eb3321752
Rutger van Bergen <rbergen@xs4all.nl>
2023-06-26 14:07:30 +0200

    Document show_envs.py

--- tools/show_envs.py
+++ tools/show_envs.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 
+# This script outputs a JSON array with all the environment names defined in platformio.ini.
+# It is used by this project GitHub CI workflow.
+#
+# Note that it expects to be executed from the project root directory. That is, it needs to be
+# run like this:
+#
+# $ tools/show_envs.py
+#
+# Instead of:
+#
+# $ cd tools
+# $ ./show_envs.py
+#
+
 import configparser
 
 config = configparser.ConfigParser()
 config.read('platformio.ini')
 
 envs = ''
-
 
 for env in config.sections():
     if env.startswith('env:'):
