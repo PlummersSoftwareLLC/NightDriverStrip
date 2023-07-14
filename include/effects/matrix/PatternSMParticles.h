bfaada44be1e36b9f1181b85876bb17a2dca7e7f
Robert Lipe <robertlipe@gmail.com>
2023-07-13 23:22:46 -0500

    Finalize particles effect. Fix rectangular grids.

--- include/effects/matrix/PatternSMParticles.h
+++ include/effects/matrix/PatternSMParticles.h
@@ -3,11 +3,30 @@
 #include "effectmanager.h"
 #include "effects/strip/musiceffect.h"
 
+// Derived from
+// https://editor.soulmatelights.com/gallery/2358-particles
+//
+// I have stared and stared at this code. I'm not at all convinced
+// this was designed as much as someone typed magic numbers that
+// looked "good" on their screen geometry.
+//
+// 'now_weekMs' relies on millis() generating basically a raster dot
+//  that indexes into leds[]
+//
+// It generates wildly out-of-bounds accesses and _relies_ on
+// drawPixels() to throw them away. Over half the drawn pixels are
+// discarded in some geometries. I found the original had X and Y
+// swapped in a few places. Fixing that REALLY helped our rectangular
+// screens. The draw loop does a RIDICULOUS amount of math just to
+// add +/1 randomly to pixels. (Or just keep them as floats, add whatever
+// to them, and just snap to the integer grid on draw, which sould allow
+// dithering....)
+//
+// I could (and probably should) have rewritten this in less time.
+
+
 // Good freakin' grief.
 #define now_weekMs 0 * 1000ul + millis()  // - tmr
-
-#define FOR_i(x, y) for (int i = (x); i < (y); i++)
-#define CUR_PRES preset[cfg_curPreset
 
 #if ENABLE_AUDIO
 class PatternSMParticles : public BeatEffectBase,
@@ -19,7 +38,7 @@ class PatternSMParticles : public LEDStripEffect
  private:
   const int thisScale = 254;  // 254 - максимальный, наверное, масштаб
   const int CUR_PRES_speed =
-      15;  // 15 - скорость, на которой частицы пляшут туда-сюда
+      25;  // 15 - скорость, на которой частицы пляшут туда-сюда
   const int CUR_PRES_color =
       1;  // 1 - цвет частиц, чтобы не заморачиваться с палитрами
 
@@ -57,26 +76,26 @@ class PatternSMParticles : public LEDStripEffect
 #if ENABLE_AUDIO
     ProcessAudio();
 #endif
+    fadeAllChannelsToBlackBy(80);
 
-    fadeAllChannelsToBlackBy(70);
+    // This relies on 'now_weekMs' changing on every loop iteration.
     byte amount = (thisScale >> 3) + 1;
     for (int i = 0; i < amount; i++) {
       int homeX = inoise16(i * 100000000ul + (now_weekMs << 3) * CUR_PRES_speed / 255);
-      homeX = map(homeX, 15000, 50000, 0, cfg_length);
+      homeX = map(homeX, 15000, 50000, 0, cfg_width);
       int offsX = inoise8(i * 2500 + (now_weekMs >> 1) * CUR_PRES_speed / 255) - 128;
-      offsX = cfg_length / 2 * offsX / 128;
+      offsX = cfg_width / 2 * offsX / 128;
       unsigned int thisX = homeX + offsX;
 
       int homeY = inoise16(i * 100000000ul + 2000000000ul +
                            (now_weekMs << 3) * CUR_PRES_speed / 255);
-      homeY = map(homeY, 15000, 50000, 0, cfg_width);
+      homeY = map(homeY, 15000, 50000, 0, cfg_length);
       int offsY = inoise8(i * 2500 + 30000 + (now_weekMs >> 1) * CUR_PRES_speed / 255) - 128;
       offsY = cfg_length / 2 * offsY / 128;
       int thisY = homeY + offsY;
       drawPixelXY(thisX, thisY, CRGB(CHSV(CUR_PRES_color, 255, 255)));
     }
   }
-
 #if ENABLE_AUDIO
   virtual void HandleBeat(bool bMajor, float elapsed, float span) override {}
 #endif
