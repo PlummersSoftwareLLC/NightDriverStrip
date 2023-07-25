045f64a06b8d6c6330eb3cad3c50087ff2785f56
David W Plummer <davepl@davepl.com>
2023-07-25 13:51:45 -0700

    Update flash.sh

--- samples/flash.sh
+++ samples/flash.sh
@@ -1,3 +1,5 @@
+; A sample file showing how to parallel-flash a batch of S3 based modules
+
 pio run -e ledstrip_feather -t upload -t nobuild --upload-port  192.168.8.5 &
 pio run -e ledstrip_feather -t upload -t nobuild --upload-port  192.168.8.37 &
 pio run -e ledstrip_feather -t upload -t nobuild --upload-port  192.168.8.31 &
