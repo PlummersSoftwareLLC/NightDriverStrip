9fe7213a9b4e4bc719cd7a36a02491aa9a5f47cf
Robert Lipe <robertlipe@gmail.com>
2023-08-07 04:18:21 -0500

    Keep EyeTunnel in bounds

--- include/effects/matrix/PatternSMEyeTunnel.h
+++ include/effects/matrix/PatternSMEyeTunnel.h
@@ -33,8 +33,8 @@ class PatternSMEyeTunnel : public LEDStripEffect
     for (uint8_t i = 0; i < 4; i++) {
       int16_t local_x = (x >> 8) + (i & 1);
       int16_t local_y = (y >> 8) + ((i >> 1) & 1);
+      if ((local_x < 0) || (local_x > MATRIX_WIDTH - 1) || (local_y < 0) || (local_y > MATRIX_HEIGHT - 1)) continue;
       int16_t xy = g()->xy(local_x, local_y);
-      if (xy < 0 || xy > NUM_LEDS) continue;
       g()->leds[xy].r = qadd8(g()->leds[xy].r, col->r * wu[i] >> 8);
       g()->leds[xy].g = qadd8(g()->leds[xy].g, col->g * wu[i] >> 8);
       g()->leds[xy].b = qadd8(g()->leds[xy].b, col->b * wu[i] >> 8);
