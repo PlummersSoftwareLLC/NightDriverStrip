eed4e8b76ac5fc33c447768f1b528f4018e0d7aa
Rutger van Bergen <rbergen@xs4all.nl>
2023-08-10 19:25:47 +0200

    Return effect, don't enable all on creation

--- include/effectfactories.h
+++ include/effectfactories.h
@@ -110,7 +110,12 @@ class EffectFactories
 
             // Disable the effect if we have one and we were asked to do so
             if (pEffect && LoadDisabled)
+            {
+                debugI("Creating effect %d in disabled state", effectNumber);
                 pEffect->SetEnabled(false);
+            }
+
+            return pEffect;
         }
     };
 
--- src/effects.cpp
+++ src/effects.cpp
@@ -388,7 +388,7 @@ void LoadEffectFactories()
         ADD_EFFECT(EFFECT_MATRIX_SPIRO,             PatternSpiro);
         ADD_EFFECT(EFFECT_MATRIX_WAVE,              PatternWave);
         ADD_EFFECT(EFFECT_MATRIX_SWIRL,             PatternSwirl);
-        ADD_EFFECT(EFFECT_MATRIX_SERENDIPITY,       PatternSerendipity);
+        ADD_EFFECT_DISABLED(EFFECT_MATRIX_SERENDIPITY,       PatternSerendipity);
         ADD_EFFECT(EFFECT_MATRIX_MANDALA,           PatternMandala);
         ADD_EFFECT(EFFECT_MATRIX_PALETTE_SMEAR,     PatternPaletteSmear);
         ADD_EFFECT(EFFECT_MATRIX_CURTAIN,           PatternCurtain);
@@ -786,9 +786,6 @@ void EffectManager::LoadDefaultEffects()
 {
     for (const auto &numberedFactory : l_ptrEffectFactories->GetDefaultFactories())
         ProduceAndLoadDefaultEffect(numberedFactory);
-
-    for (int i = 0; i < _vEffects.size(); i++)
-        EnableEffect(i, true);
 
     SetInterval(DEFAULT_EFFECT_INTERVAL, true);
 
