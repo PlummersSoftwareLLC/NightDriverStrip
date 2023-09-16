1a9e5a8cb05e291bac7ffc7e6afa74884ad5baed
keiranhines <keiranhines@gmail.com>
2023-09-16 13:42:58 +1000

    remove styling errors and warning on drawer menu

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,6 +1,6 @@
 import MainApp from './components/home/home'
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-
-ReactDOM.createRoot(document.getElementById("root"))
-        .render(<StrictMode><MainApp/></StrictMode>);
+import { createRoot } from 'react-dom/client';
+createRoot(document.getElementById("root"))
+  .render(<StrictMode><MainApp/></StrictMode>);
