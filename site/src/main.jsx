1a8cb276f1d27adb6915f9d4a1536672de6f266c
keiranhines <keiranhines@gmail.com>
2023-09-18 21:57:56 +1000

    Updated readme and bake_site to include details for the requirement of installing NPM

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import MainApp from './components/home/home'
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root"))
-    .render(<StrictMode><MainApp/></StrictMode>);
+ReactDOM.render(
+    <React.StrictMode>
+        <MainApp />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
