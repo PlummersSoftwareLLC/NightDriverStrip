2b864806cda7d8b1dc064a4d6cda4715e2fd2d10
Robert Lipe <robertlipe@gmail.com>
2023-09-28 01:16:17 -0500

    Visual fixes to SMSmoke

--- include/effects/matrix/PatternSMSmoke.h
+++ include/effects/matrix/PatternSMSmoke.h
@@ -42,7 +42,8 @@ public:
       hue2 = 0U;
       hue = random8();
     }
-    if (deltaHue & 0x01) //((deltaHue >> 2U) == 0U) // какой-то умножитель охота, подключить к задержке смены цвета, но хз какой...
+    if (deltaHue & 0x01) //((deltaHue >> 2U) == 0U) // (orig) I'd like to connect some kind of multiplier to the color change delay, but I don't know what...
+
       hue2++;
 
     if (g()->IsPalettePaused())
@@ -57,25 +58,41 @@ public:
     }
 
     // deltaHue2--;
-    if (random8(WIDTH) != 0U) // встречная спираль движется не всегда синхронно основной
+    if (random8(WIDTH) != 0U) // (orig) // the counter spiral does not always move synchronously with the main one.
       deltaHue2--;
 
+    // Two diagonal (note Y is used for height AND X offset)
+    // "wipers", of different colors. Each leaves a
+    // sky-written trailer of color.
+    // slew is used to make the lines look less mathematically diagonal
+    // and introduce some delay between color1 and color2. This gives a
+    // "tear" in the refresh and as a bonus, gives some 'pepper in fire'
+    // effect to he colors which gets quickly blended.
     for (uint8_t y = 0; y < HEIGHT; y++)
     {
-      g()->leds[XY((deltaHue + y + 1U) % WIDTH, HEIGHT - 1U - y)] += color;
-      g()->leds[XY((deltaHue + y) % WIDTH, HEIGHT - 1U - y)] += color2; // color2
-      g()->leds[XY((deltaHue2 + y) % WIDTH, y)] += color;
-      g()->leds[XY((deltaHue2 + y + 1U) % WIDTH, y)] += color2; // color2
+      uint8_t slew = random8(8) - 8/2 + 1;
+      uint8_t slew2 = random8(8) - 8/2 + 1;
+      g()->leds[XY((deltaHue + y + slew + 1U) % WIDTH, HEIGHT - 1U - y)] += color;
+      g()->leds[XY((deltaHue + y + slew2) % WIDTH, HEIGHT - 1U - y)] += color2; // color2
+      g()->leds[XY((deltaHue2 + y + slew2) % WIDTH, y)] += color;
+      g()->leds[XY((deltaHue2 + y + slew + 1U) % WIDTH, y)] += color2; // color2
     }
 
     EVERY_N_MILLISECONDS(100)
     {
-      g()->SetNoise(1000, 1000, 1000, 4000, 4000);
+      // (orig) "speed of movement through the noise array"
+      // Calling SetNoise() in here will index past what was
+      // FillGetNoised, which returns slowly scrolling bars
+      // of black along X and Y axes.
       g()->FillGetNoise();
+      // g()->SetNoise(1, 1, 1, 4, 4);
     }
 
+    // Lower number for thicker, more static fog. Higher for more wisp.
     g()->MoveFractionalNoiseX(3);
     g()->MoveFractionalNoiseY(3);
-    g()->BlurFrame(20);
+    // Without this, we get tornadoes where the diagonals cross as there's
+    // an excess of set pixels there.
+    g()->BlurFrame(10);
   }
 };
