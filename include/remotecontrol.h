6db04bfb120c9de8721a9959c5fe912af9b3f181
Joe Schneider <joe@revision29.com>
2023-10-06 23:57:36 -0400

    Added button defintions
    Added additional key definitions for 24 and 44 key remotes so that keys not present won't crash the device. I also added colors that correspond to the colors on my remote.

--- include/remotecontrol.h
+++ include/remotecontrol.h
@@ -36,8 +36,13 @@
 #include <limits>
 
 
-#define key24  true
-#define key44  false
+//#define key24  true
+//#define key44  false
+
+#define key24  false
+#define key44  true
+
+
 
 void IRAM_ATTR RemoteLoopEntry(void *);
 
@@ -66,53 +71,94 @@ void IRAM_ATTR RemoteLoopEntry(void *);
 #define IR_B11    0xF7A857  //
 #define IR_B12    0xF76897  //
 #define IR_SMOOTH 0xF7E817  //
+//The following are to keep code from breaking due to undefined constants
+#define IR_B13    0x000000  //
+#define IR_B14    0x000000  //
+#define IR_B15    0x000000  //
+#define IR_B16    0x000000  //
+#define IR_FLASH  0x000000  //
+#define IR_JUMP3  0x000000  //
+#define IR_JUMP7  0x000000  //
+#define IR_FADE3  0x000000  //
+#define IR_FADE7  0x000000  //
+#define IR_UPR    0x000000  //
+#define IR_UPG    0x000000  //
+#define IR_UPB    0x000000  //
+#define IR_QUICK  0x000000  //
+#define IR_DOWNR  0x000000  //
+#define IR_DOWNG  0x000000  //
+#define IR_DOWNB  0x000000  //
+#define IR_SLOW   0x000000  //
+#define IR_DIY1   0x000000  //
+#define IR_DIY2   0x000000  //
+#define IR_DIY3   0x000000  //
+#define IR_AUTO   0x000000  //
+#define IR_DIY4   0x000000  //
+#define IR_DIY5   0x000000  //
+#define IR_DIY6   0x000000  //
+
 #endif
 
 #if key44
-#define IR_BPlus  0xFF3AC5  //
-#define IR_BMinus 0xFFBA45  //
+#define IR_BPLUS  0xFF3AC5  //Row 1
+#define IR_BMINUS 0xFFBA45  //
 #define IR_ON     0xFF827D  //
 #define IR_OFF    0xFF02FD  //
-#define IR_R      0xFF1AE5  //
+#define IR_R      0xFF1AE5  //Row 2
 #define IR_G      0xFF9A65  //
 #define IR_B      0xFFA25D  //
 #define IR_W      0xFF22DD  //
-#define IR_B1     0xFF2AD5  //
+
+#define IR_B1     0xFF2AD5  //Row 3
 #define IR_B2     0xFFAA55  //
 #define IR_B3     0xFF926D  //
 #define IR_B4     0xFF12ED  //
-#define IR_B5     0xFF0AF5  //
+
+#define IR_B5     0xFF0AF5  //Row 4
 #define IR_B6     0xFF8A75  //
 #define IR_B7     0xFFB24D  //
 #define IR_B8     0xFF32CD  //
-#define IR_B9     0xFF38C7  //
+
+#define IR_B9     0xFF38C7  //Row 5
 #define IR_B10    0xFFB847  //
 #define IR_B11    0xFF7887  //
 #define IR_B12    0xFFF807  //
-#define IR_B13    0xFF18E7  //
+
+#define IR_B13    0xFF18E7  //Row 6
 #define IR_B14    0xFF9867  //
 #define IR_B15    0xFF58A7  //
 #define IR_B16    0xFFD827  //
-#define IR_UPR    0xFF28D7  //
-#define IR_UPG    0xFFA857  //
-#define IR_UPB    0xFF6897  //
-#define IR_QUICK  0xFFE817  //
-#define IR_DOWNR  0xFF08F7  //
-#define IR_DOWNG  0xFF8877  //
-#define IR_DOWNB  0xFF48B7  //
-#define IR_SLOW   0xFFC837  //
-#define IR_DIY1   0xFF30CF  //
-#define IR_DIY2   0xFFB04F  //
-#define IR_DIY3   0xFF708F  //
-#define IR_AUTO   0xFFF00F  //
-#define IR_DIY4   0xFF10EF  //
-#define IR_DIY5   0xFF906F  //
-#define IR_DIY6   0xFF50AF  //
-#define IR_FLASH  0xFFD02F  //
-#define IR_JUMP3  0xFF20DF  //
-#define IR_JUMP7  0xFFA05F  //
-#define IR_FADE3  0xFF609F  //
-#define IR_FADE7  0xFFE01F  //
+//Row 7
+#define IR_JUMP3  0xFF28D7  //This jump cuts between three colors quickly.
+#define IR_JUMP7  0xFFA857  //This jump cuts between 7 colors quickly.
+#define IR_FADE3  0xFF6897  //This fades between three colors.
+#define IR_FADE7  0xFFE817  //this fades between 7 colors quickly.
+//Row 8
+#define IR_UPR    0xFF08F7  //
+#define IR_UPG    0xFF8877  //
+#define IR_UPB    0xFF48B7  //
+#define IR_QUICK  0xFFC837  //Fades between all colors quickly.
+//Row 9
+#define IR_DOWNR  0xFF30CF  //
+#define IR_DOWNG  0xFFB04F  //
+#define IR_DOWNB  0xFF708F  //
+#define IR_SLOW   0xFFF00F  //
+//Row 10
+#define IR_DIY1   0xFF10EF  //This should be factored to trigger an effect the user has added to the effects manager.
+#define IR_DIY2   0xFF906F  //
+#define IR_DIY3   0xFF50AF  //
+#define IR_AUTO   0xFFD02F  //
+//Row 11
+#define IR_DIY4   0xFF20DF  //
+#define IR_DIY5   0xFFA05F  //
+#define IR_DIY6   0xFF609F  //
+#define IR_FLASH  0xFFE01F  //
+//Define these for compatibility and not break code
+#define IR_SMOOTH 0x000000  //
+#define IR_STROBE 0x000000  //
+#define IR_FADE 0x000000  //
+
+
 #endif
 
 const static struct
@@ -121,6 +167,66 @@ const static struct
     CRGB    color;
     uint8_t hue;
 }
+
+#if key44
+//A person will want to go through the trouble of setting color values that approximate the colors printed in their remote. *Approxminate* because print colors != light colors.
+ RemoteColorCodes[] =
+{
+    { IR_OFF, CRGB(000, 000, 000), 0    },
+    { IR_R,   CRGB(255, 000, 000), 0    },
+    { IR_G,   CRGB(000, 255, 000), 96   },
+    { IR_B,   CRGB(000, 000, 255), 160  },
+    { IR_W,   CRGB(255, 255, 255), 0    },
+
+    { IR_B1,  CRGB(217,  217, 18), 23}, //EBEBEB
+    { IR_B2,  CRGB(28, 149,  5), 78  }, //1B9205
+    { IR_B3,  CRGB( 21, 17, 111), 172  }, //170C96
+    { IR_B4,  CRGB(240, 180, 215), 231}, //F1BFDC
+
+    { IR_B5,  CRGB(241, 184, 71), 28  }, //FBBE56
+    { IR_B6,  CRGB(25, 141, 66), 100  }, //229248
+    { IR_B7,  CRGB(25, 2, 130), 178   }, //200991
+    { IR_B8,  CRGB(229, 180, 203), 235  }, //D72FB9
+   
+    { IR_B9,  CRGB(240, 199, 71), 32  }, //EDD917
+    { IR_B10,  CRGB(20, 133, 133), 128  },//2A92A1
+    { IR_B11,  CRGB(117, 18, 188), 195 },//7415B4
+    { IR_B12,  CRGB(47, 164, 182), 133 }, //39AAC3
+
+    { IR_B13,  CRGB(216, 230, 14), 45 }, //D0E30F
+    { IR_B14,  CRGB(45, 125, 200), 148 }, //2E7CC7
+    { IR_B15,  CRGB(186, 33, 171), 217},//C121B1
+    { IR_B16,  CRGB(47, 122, 206), 150}//2D87D7
+};
+#else
+//These are the default colors for 24 button remotes or whatever remote a person chooses.
+    RemoteColorCodes[] =
+{
+    { IR_OFF, CRGB(000, 000, 000), 0    },
+
+    { IR_R,   CRGB(255, 000, 000), 0    },
+    { IR_G,   CRGB(000, 255, 000), 96   },
+    { IR_B,   CRGB(000, 000, 255), 160  },
+    { IR_W,   CRGB(255, 255, 255), 0    },
+
+    { IR_B1,  CRGB(255,  64, 000), 16   },
+    { IR_B2,  CRGB(000, 255,  64), 112  },
+    { IR_B3,  CRGB( 64, 000, 255), 176  },
+
+    { IR_B4,  CRGB(255, 128, 000), 32   },
+    { IR_B5,  CRGB(000, 255, 128), 128  },
+    { IR_B6,  CRGB(128, 000, 255), 192  },
+
+    { IR_B7,  CRGB(255, 192, 000), 48   },
+    { IR_B8,  CRGB(000, 255, 192), 112  },
+    { IR_B9,  CRGB(192, 000, 255), 208  },
+
+    { IR_B10,  CRGB(255, 255, 000), 64  },
+    { IR_B11,  CRGB(000, 255, 255), 144 },
+    { IR_B12,  CRGB(255, 000, 255), 224 }
+};
+#endif
+/*
 RemoteColorCodes[] =
 {
     { IR_OFF, CRGB(000, 000, 000), 0    },
@@ -146,14 +252,14 @@ RemoteColorCodes[] =
     { IR_B11,  CRGB(000, 255, 255), 144 },
     { IR_B12,  CRGB(255, 000, 255), 224 }
 };
-
+*/
 class RemoteControl
 {
   private:
     IRrecv _IR_Receive;
 
   public:
-
+  
     RemoteControl() : _IR_Receive(IR_REMOTE_PIN)
     {
     }
@@ -173,5 +279,8 @@ class RemoteControl
 
     void handle();
 };
+
+
+
 
 #endif
