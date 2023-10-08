a04a53e2dff297416c2fd874241ce1cfdeff68de
Joe Schneider <joe@revision29.com>
2023-10-08 16:38:15 -0400

    Updated ledstripgfx.cpp to apply system brightness to pixels.
    After the pixels are added to FastLED, we iterate each one and apply the system level brightness.

--- src/ledstripgfx.cpp
+++ src/ledstripgfx.cpp
@@ -53,9 +53,13 @@ void LEDStripGFX::PostProcessFrame(uint16_t wifiPixelsDrawn, uint16_t localPixel
 
     auto& effectManager = g_ptrSystem->EffectManager();
 
-    for (int i = 0; i < NUM_CHANNELS; i++)
+    for (int i = 0; i < NUM_CHANNELS; i++) {
         FastLED[i].setLeds(effectManager.g(i)->leds, pixelsDrawn);
-    FastLED.show(g_Values.Fader);
+        for (int j = 0; j < FastLED[i].size(); j++){
+            FastLED[i][j].fadeLightBy(255-g_ptrSystem->DeviceConfig().GetBrightness());
+        }
+    }
+    FastLED.show(g_Values.Fader); //Shows the pixels
 
     g_Values.FPS = FastLED.getFPS();
     g_Values.Brite = 100.0 * calculate_max_brightness_for_power_mW(g_ptrSystem->DeviceConfig().GetBrightness(), POWER_LIMIT_MW) / 255;
