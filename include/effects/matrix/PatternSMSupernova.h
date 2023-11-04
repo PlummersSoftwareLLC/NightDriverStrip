bdc6bbc54fe1a2b4a32a865ba2ea2dfe0776af08
Robert Lipe <robertlipe@gmail.com>
2023-11-04 08:28:00 -0500

    Cosmetic improvements
    Reduce scope of struct helper class.
    Rename Object -> DebrisItem
    Try to make capitalization (struct membbers vs. class vs. loals) consistent.
    Framerate of 120 was delusional fantasy. 60 is at least reasonable fantasy.
    enlargedObjectNUM: remove write-only variable

--- include/effects/matrix/PatternSMSupernova.h
+++ include/effects/matrix/PatternSMSupernova.h
@@ -4,93 +4,89 @@
 
 // Inspired by https://editor.soulmatelights.com/gallery/1923-supernova
 
-// A more cache-friendly version of the 8 independent arrays that were
-// used. This allows better locality per member, especially since we
-// access them sequentially.
-// Nothing special here; just default-zero initialized struct.
-class Object {
-  public:
-    void Objects() {
-        PosX = 0.0f;
-        PosY = 0.0f;
-        State = 0.0f;
-        SpeedX = 0.0f;
-        SpeedY = 0.0f;
-        Hue = 0;
-        IsShift = false;
-    }
-
-    float PosX;
-    float PosY;
-    float SpeedX;
-    float SpeedY;
-    uint8_t Hue;
-    uint8_t State;
-    bool IsShift;
-};
 
 class PatternSMSupernova : public LEDStripEffect
 {
 public:
 
-    PatternSMSupernova() : LEDStripEffect(EFFECT_MATRIX_SMSUPERNOVA, "Supernova"), hue(0), hue2(0), step(0), deltaValue(0), enlargedObjectNUM(0)
+    PatternSMSupernova() : LEDStripEffect(EFFECT_MATRIX_SMSUPERNOVA, "Supernova"), hue(0), hue2(0), step(0), enlargedDebrisItemNUM(0)
     {
     }
 
-    PatternSMSupernova(const JsonObjectConst &jsonObject) : LEDStripEffect(jsonObject)
+    PatternSMSupernova(const JsonObjectConst &jsonDebrisItem) : LEDStripEffect(jsonDebrisItem)
     {
     }
 
     virtual size_t DesiredFramesPerSecond() const override
     {
-        return 120;
+        return 60;
     }
 
     void Start() override
     {
         g()->Clear();
-        enlargedObjectNUM = 200;
-        if (enlargedObjectNUM > MAX_COUNT)
-            enlargedObjectNUM = 200U;
-        deltaValue = enlargedObjectNUM /
-            (sqrt3(CENTER_X_MAJOR * CENTER_X_MAJOR +
-                   CENTER_Y_MAJOR * CENTER_Y_MAJOR) * 4U) + 1U;
+        enlargedDebrisItemNUM = 200;
+        if (enlargedDebrisItemNUM > MAX_COUNT)
+            enlargedDebrisItemNUM = 200U;
     }
 
     void Draw() override {
         step = -1;
         g()->DimAll(200);
 
-        for (auto& object : Objects) {
-            if (!object.IsShift && step) {
-                starfield2Emit(object);
+        for (auto& debris_item : _debris_items) {
+            if (!debris_item._is_shift && step) {
+                StarfieldEmit(debris_item);
                 step -= 1;
             }
 
-            if (object.IsShift) {
-                particlesUpdate2(object);
-                // pU2() may have updated the object beyond bounds and
-                // cleared IsShift. Retest it and ignore if so.
+            if (debris_item._is_shift) {
+                ParticlesUpdate(debris_item);
+                // pU2() may have updated the debris_item beyond bounds and
+                // cleared _is_shift. Retest it and ignore if so.
                 // Alternate design could use pU2()'s return value for this.
-                if (object.IsShift == false) continue;;
+                if (debris_item._is_shift == false) continue;;
 
-                CRGB baseRGB = ColorFromPalette(HeatColors_p, object.Hue,
+                CRGB baseRGB = ColorFromPalette(HeatColors_p, debris_item._hue,
                                                 255, LINEARBLEND);
-                baseRGB.nscale8(object.State);
-                drawPixelXYF(object.PosX, object.PosY, baseRGB);
+                baseRGB.nscale8(debris_item._state);
+                drawPixelXYF(debris_item._position_x, debris_item._position_y, baseRGB);
             }
         }
     }
 
 private:
-    const uint8_t CENTER_X_MINOR = (MATRIX_WIDTH / 2) - ((MATRIX_WIDTH - 1) & 0x01);
-    const uint8_t CENTER_Y_MINOR = (MATRIX_HEIGHT / 2) - ((MATRIX_HEIGHT - 1) & 0x01);
-    const uint8_t CENTER_X_MAJOR = MATRIX_WIDTH / 2 + (MATRIX_WIDTH % 2);
-    const uint8_t CENTER_Y_MAJOR = MATRIX_HEIGHT / 2 + (MATRIX_HEIGHT % 2);
-    uint8_t hue, hue2, step, deltaValue, enlargedObjectNUM;
+    // A more cache-friendly version of the 8 independent arrays that were
+    // used. This allows better locality per member, especially since we
+    // access them sequentially.
+    // Nothing special here; just a default-zero initialized struct.
+    class DebrisItem
+    {
+      public:
+        DebrisItem()
+        {
+            _position_x = 0.0f;
+            _position_y = 0.0f;
+            _state = 0.0f;
+            _speed_x = 0.0f;
+            _speed_y = 0.0f;
+            _hue = 0;
+            _is_shift = false;
+        }
+
+        float _position_x;
+        float _position_y;
+        float _speed_x;
+        float _speed_y;
+        uint8_t _hue;
+        uint8_t _state;
+        bool _is_shift;
+    };
+
+    uint8_t hue, hue2, step, enlargedDebrisItemNUM;
 
     static constexpr int MAX_COUNT = 254;
-    std::array<Object, MAX_COUNT> Objects;
+    std::array<DebrisItem, MAX_COUNT> _debris_items;
 
     // Fast Babylonian Approximate square root.
     // It's called one time in the constructor, so no need for
@@ -107,31 +103,31 @@ private:
         return u.x;
     }
 
-    void inline particlesUpdate2(Object& object)
+    void inline ParticlesUpdate(DebrisItem& debris_item)
     {
         // Intentionally do a narrowing conversion here.
-        const int x = object.PosX += object.SpeedX;
-        const int y = object.PosY += object.SpeedY;
+        const int x = debris_item._position_x += debris_item._speed_x;
+        const int y = debris_item._position_y += debris_item._speed_y;
 
-        if (object.State == 0 || x < 0 || x >= MATRIX_WIDTH ||
+        if (debris_item._state == 0 || x < 0 || x >= MATRIX_WIDTH ||
                                  y < 0 || y >= MATRIX_HEIGHT)
-            object.IsShift = false;
+            debris_item._is_shift = false;
     }
 
-    void inline starfield2Emit(Object& object)
+    void inline StarfieldEmit(DebrisItem& debris_item)
     {
         if (hue++ & 0x01)
             hue2 += 1;
-        object.PosX = MATRIX_WIDTH * 0.5;
-        object.PosY = MATRIX_HEIGHT * 0.5;
-        object.SpeedX = (((float)random8() - 127.) / 512.);
-        object.SpeedY = sqrt(0.0626 - object.SpeedX * object.SpeedX);
+        debris_item._position_x = MATRIX_WIDTH * 0.5;
+        debris_item._position_y = MATRIX_HEIGHT * 0.5;
+        debris_item._speed_x = (((float)random8() - 127.) / 512.);
+        debris_item._speed_y = sqrtf(0.0626f - debris_item._speed_x * debris_item._speed_y);
         if (random8(2U)) {
-            object.SpeedY = -object.SpeedY;
+            debris_item._speed_y = -debris_item._speed_y;
         }
-        object.State = random8(1, 250);
-        object.Hue = hue2;
-        object.IsShift = true;
+        debris_item._state = random8(1, 250);
+        debris_item._hue = hue2;
+        debris_item._is_shift = true;
     }
 
     static inline uint8_t WU_WEIGHT(uint8_t a, uint8_t b)
