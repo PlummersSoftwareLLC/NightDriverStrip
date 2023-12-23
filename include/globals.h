2cac1000848c416b231eae1b363089322c6ee37c
Rutger van Bergen <rbergen@xs4all.nl>
2023-12-23 21:19:38 +0100

    Update ESP Web Tools

--- include/globals.h
+++ include/globals.h
@@ -950,15 +950,15 @@ extern RemoteDebug Debug;           // Let everyone in the project know about it
 
     #define ENABLE_AUDIOSERIAL          0   // Report peaks at 2400baud on serial port for PETRock consumption
     #define ENABLE_WIFI                 1   // Connect to WiFi
-    #define INCOMING_WIFI_ENABLED       1   // Accepting incoming color data and commands
+    #define INCOMING_WIFI_ENABLED       0   // Accepting incoming color data and commands
     #define WAIT_FOR_WIFI               0   // Hold in setup until we have WiFi - for strips without effects
     #define TIME_BEFORE_LOCAL           2   // How many seconds before the lamp times out and shows local content
-    #define MAX_BUFFERS                 5   // Keep buffers low because we have little memory to work with
+    #define MAX_BUFFERS                 3   // Keep buffers low because we have little memory to work with
 
-    #define ENABLE_WEBSERVER            1   // Turn on the internal webserver
+    #define ENABLE_WEBSERVER            0   // Turn on the internal webserver
     #define ENABLE_NTP                  1   // Set the clock from the web
     #define ENABLE_OTA                  0   // Accept over the air flash updates
-    #define ENABLE_REMOTE               1   // IR Remote Control
+    #define ENABLE_REMOTE               0   // IR Remote Control
     #define ENABLE_AUDIO                1   // Listen for audio from the microphone and process it
     #define COLORDATA_SERVER_ENABLED    0
 
