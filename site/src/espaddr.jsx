39e9455e4dfdcd48a05e22306245b88734c2696d
keiranhines <keiranhines@gmail.com>
2023-12-31 10:28:53 +1100

    Fixed issue so ws can load on a flashed board

--- site/src/espaddr.jsx
+++ site/src/espaddr.jsx
@@ -1,6 +1,6 @@
-const address = process.env.NODE_ENV === "development" ? "10.1.5.1" : undefined;
+const address = process.env.NODE_ENV === "development" ? "255.255.255.0" : undefined;
 const httpPrefix = address ? `http://${address}`: undefined
-const wsPrefix = address ? `ws://${address}/ws`: undefined
+const wsPrefix = address ? `ws://${address}/ws`: `ws://${window.location.host}/ws` 
 
 export default httpPrefix;
 export {address, httpPrefix, wsPrefix}
