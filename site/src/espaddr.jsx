6f940a3732046e7505da4f9f296c36c9ad13dc4f
Keiran Hines <keiranhines@gmail.com>
2025-03-31 19:59:52 +1100

    Fix: cross origin loading issue

--- site/src/espaddr.jsx
+++ site/src/espaddr.jsx
@@ -1,6 +1,6 @@
 const address = process.env.NODE_ENV === "development" ? "255.255.255.0" : undefined;
-const httpPrefix = address ? `http://${address}`: undefined
-const wsPrefix = address ? `ws://${address}/ws`: `ws://${window.location.host}/ws` 
+const httpPrefix = address ? `http://${address}` : undefined
+const wsPrefix = address ? `ws://${address}/ws` : `ws://${window.location.host}/ws`
 
 export default httpPrefix;
-export {address, httpPrefix, wsPrefix}
+export { address, httpPrefix, wsPrefix }
