6cf1da951092820a35b10d0c4adcced19632d5f8
Keiran Hines <keiranhines@gmail.com>
2025-04-18 20:00:07 +1000

    Feat: Split calls for statistics into static and dynamic

--- site/src/context/statsContext.jsx
+++ site/src/context/statsContext.jsx
@@ -3,29 +3,66 @@ import httpPrefix from "../espaddr";
 import PropTypes from 'prop-types';
 
 const StatsContext = createContext(undefined);
-const statsEndpoint = `${httpPrefix !== undefined ? httpPrefix : ""}/statistics`;
+const statsEndpoint = `${httpPrefix !== undefined ? httpPrefix : ""}/statistics/static`;
 
 const StatsProvider = ({ children }) => {
-    const [matrixWidth, setMatrixWidth] = useState(64);
-    const [matrixHeight, setMatrixHeight] = useState(32);
-    const [framesSocket, setFramesSocket] = useState(false);
+    const [chipCores, setChipCores] = useState(1);
+    const [chipModel, setChipModel] = useState('ESP32');
+    const [chipSpeed, setChipSpeed] = useState(240);
+    const [codeFree, setCodeFree] = useState(0);
+    const [codeSize, setCodeSize] = useState(0);
+    const [dmaSize, setDmaSize] = useState(0);
     const [effectsSocket, setEffectsSocket] = useState(false);
+    const [flashSize, setFlashSize] = useState(0);
+    const [framesSocket, setFramesSocket] = useState(false);
+    const [heapSize, setHeapSize] = useState(0);
+    const [matrixHeight, setMatrixHeight] = useState(32);
+    const [matrixWidth, setMatrixWidth] = useState(64);
+    const [progSize, setProgSize] = useState(0);
+    const [psRamSize, setPsRamSize] = useState(0);
+
     useEffect(() => {
         const getDataFromDevice = async (params) => {
             try {
                 const statistics = await fetch(statsEndpoint, params).then(r => r.json());
-                setMatrixWidth(statistics.MATRIX_WIDTH);
-                setMatrixHeight(statistics.MATRIX_HEIGHT);
-                setFramesSocket(statistics.FRAMES_SOCKET);
+                setChipCores(statistics.CHIP_CORES);
+                setChipModel(statistics.CHIP_MODEL);
+                setChipSpeed(statistics.CHIP_SPEED);
+                setCodeFree(statistics.CODE_FREE);
+                setCodeSize(statistics.CODE_SIZE);
+                setDmaSize(statistics.DMA_SIZE);
                 setEffectsSocket(statistics.EFFECTS_SOCKET);
-            } catch(err) {
+                setFlashSize(statistics.FLASH_SIZE);
+                setFramesSocket(statistics.FRAMES_SOCKET);
+                setHeapSize(statistics.HEAP_SIZE);
+                setMatrixHeight(statistics.MATRIX_HEIGHT);
+                setMatrixWidth(statistics.MATRIX_WIDTH);
+                setProgSize(statistics.PROG_SIZE);
+                setPsRamSize(statistics.PSRAM_SIZE);
+
+            } catch (err) {
                 console.debug("Aborted Statistics update");
             }
         };
         getDataFromDevice();
     });
     return (
-        <StatsContext.Provider value={{matrixWidth, matrixHeight, effectsSocket, framesSocket}}>
+        <StatsContext.Provider value={{
+            chipCores,
+            chipModel,
+            chipSpeed,
+            codeFree,
+            codeSize,
+            dmaSize,
+            effectsSocket,
+            flashSize,
+            framesSocket,
+            heapSize,
+            matrixHeight,
+            matrixWidth,
+            progSize,
+            psRamSize
+        }}>
             {children}
         </StatsContext.Provider>
     );
@@ -38,4 +75,4 @@ StatsProvider.propTypes = {
     ]).isRequired
 };
 
-export {StatsContext, StatsProvider};
+export { StatsContext, StatsProvider };
