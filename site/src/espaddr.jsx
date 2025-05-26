0155e22d57b3ec4bc675a72e24db8b08e22eaae7
Keiran Hines <keiranhines@gmail.com>
2025-05-27 08:49:18 +1000

    Revert: Changed espaddr back to 255.255.255.0

--- site/src/espaddr.jsx
+++ site/src/espaddr.jsx
@@ -1,4 +1,4 @@
-const address = process.env.NODE_ENV === "development" ? "10.1.5.1" : undefined;
+const address = process.env.NODE_ENV === "development" ? "255.255.255.0" : undefined;
 const httpPrefix = address ? `http://${address}` : undefined
 const wsPrefix = address ? `ws://${address}/ws` : `ws://${window.location.host}/ws`
 
