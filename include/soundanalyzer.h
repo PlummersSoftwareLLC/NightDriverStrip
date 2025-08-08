82728e99c0b8122ac84b577772f7c6e319273e02
Dave Plummer <davepl@davepl.com>
2025-08-07 19:02:51 -0700

    Update soundanalyzer define

--- include/soundanalyzer.h
+++ include/soundanalyzer.h
@@ -576,7 +576,10 @@ public:
         // use an analog mic connected to the input pin.
         
         i2s_config_t i2s_config;
-        i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN);
+        i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX);
+        if (SOC_I2S_SUPPORTS_ADC)
+            i2s_config.mode += I2S_MODE_ADC_BUILT_IN; // Enable ADC built-in mode if supported
+
         // Note: Post IDF4, I2S_MODE_ADC_BUILT_IN is no longer supported
         // and it was never available on models after original ESP32-Nothing.
         // See: https://github.com/espressif/arduino-esp32/issues/9564
