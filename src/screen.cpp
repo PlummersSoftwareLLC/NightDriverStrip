ea7ae0447e7f34efcaf501fa5805104c0d7a7bbe
Dave Plummer <davepl@davepl.com>
2025-09-06 12:16:43 -0700

    Swap button roles on M5Stack devices

--- src/screen.cpp
+++ src/screen.cpp
@@ -654,16 +654,16 @@ void IRAM_ATTR Screen::RunUpdateLoop()
         #if USE_M5
             // Use M5Unified button abstraction when available
             M5.update();
-            if (M5.BtnA.wasPressed())
-            {
-                auto &pages = Pages();
-                pages[g_iCurrentPage]->OnButtonPress(0); // BtnA -> button 0 (flip page by default)
-                bRedraw = true;
-            }
             if (M5.BtnB.wasPressed())
             {
                 auto &pages = Pages();
-                pages[g_iCurrentPage]->OnButtonPress(1); // BtnB -> button 1 (next effect by default)
+                pages[g_iCurrentPage]->OnButtonPress(0); // BtnA -> button 0
+                bRedraw = true;
+            }
+            if (M5.BtnA.wasPressed())
+            {
+                auto &pages = Pages();
+                pages[g_iCurrentPage]->OnButtonPress(1); // BtnB -> button 1
                 bRedraw = true;
             }
         #endif
