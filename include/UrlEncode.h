6bd5cfb3c4797ca85830da0ebe2a1358e286001e
Robert Lipe <robertlipe@gmail.com>
2026-01-05 01:43:46 -0600

    Replace UrlEncode library with local implementation.

---
+++ include/UrlEncode.h
@@ -0,0 +1,34 @@
+//+--------------------------------------------------------------------------
+//
+// File:        UrlEncode.h
+//
+// NightDriverStrip - (c) 2026 Plummer's Software LLC.  All Rights Reserved.
+//
+// This file is part of the NightDriver software project.
+//
+//    NightDriver is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    NightDriver is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Nightdriver.  It is normally found in copying.txt
+//    If not, see <https://www.gnu.org/licenses/>.
+//
+// Description:
+//
+//   Helper to URL-encode strings for API calls.
+//   Implemenation provides RFC 1738 style percent-encoding.
+//
+//---------------------------------------------------------------------------
+
+#pragma once
+
+#include <Arduino.h>
+
+String urlEncode(const String& str);
